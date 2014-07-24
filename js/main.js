2b46d9e7f3da4a898dd55b76c7fb26d778830351
Rich Trott <rtrott@gmail.com>
2014-07-23 22:36:35 -0700

    add linting pre-commit hook

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-var id = location.search.substring(1).replace(/\W/g, '') || "1";
+/* global window */
+/* global d3 */
+var id = window.location.search.substring(1).replace(/\W/g, '') || '1';
 d3.json('data/' + id + '.json', function (error, links) {
   if (error) {
-    d3.select(".visualization").remove();
-    var body = d3.select("body");
-    body.append("p").text("Something went wrong.");
-    body.append("p").text("Check your network connection and reload the page.");
-    body.append("p").text("If the problem persists, you can email rtrott@gmail.com and I'll try to fix it.");
-    body.append("p").text("If you do that, please include as much detail as you can:");
-    var ul = body.append("ul");
-    ul.append("li").text("what you did leading up to the problem");
-    ul.append("li").text("the current URL of this page");
-    ul.append("li").text("anything else you might think would be relevant");
-    body.append("p").text("Thanks!");
+    d3.select('.visualization').remove();
+    var body = d3.select('body');
+    body.append('p').text('Something went wrong.');
+    body.append('p').text('Check your network connection and reload the page.');
+    body.append('p').text('If the problem persists, you can email rtrott@gmail.com and I\'ll try to fix it.');
+    body.append('p').text('If you do that, please include as much detail as you can:');
+    var ul = body.append('ul');
+    ul.append('li').text('what you did leading up to the problem');
+    ul.append('li').text('the current URL of this page');
+    ul.append('li').text('anything else you might think would be relevant');
+    body.append('p').text('Thanks!');
     return error;
   }
 
@@ -22,39 +24,41 @@ d3.json('data/' + id + '.json', function (error, links) {
         words = text.text().split(/\s+/).reverse(),
         word,
         line = [],
-        y = text.attr("y"),
-        tspan = text.text(null).append("tspan").attr("x", 0);
-      while (word = words.pop()) {
+        y = text.attr('y'),
+        tspan = text.text(null).append('tspan').attr('x', 0);
+      
+      while (words.length) {
+        word = words.pop();
         line.push(word);
-        tspan.text(line.join(" "));
+        tspan.text(line.join(' '));
         if (tspan.node().getComputedTextLength() > width && line.length > 1) {
           y = y-10;
-          text.attr("y", y);
+          text.attr('y', y);
           line.pop();
-          tspan.text(line.join(" "));
+          tspan.text(line.join(' '));
           line = [word];
-          tspan = text.append("tspan").attr("x", x).attr("dy", 18).text(word);
+          tspan = text.append('tspan').attr('x', x).attr('dy', 18).text(word);
         }
       }
     });
   };
 
   var transform = function (d) {
-    return "translate(" + d.x + "," + d.y + ")";
+    return 'translate(' + d.x + ',' + d.y + ')';
   };
 
   var textTransform = function (d) {
     var y = d.y + 8;
-    return "translate(" + d.x + "," + y + ")";
+    return 'translate(' + d.x + ',' + y + ')';
   };
 
   var tick = function () {
-    nodes.attr("transform", transform);
-    text.attr("transform", textTransform);
+    nodes.attr('transform', transform);
+    text.attr('transform', textTransform);
   };
 
   var zoom = function () {
-    svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    svg.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
   };
 
   var vertices = [{name: links.source, trackCount: links.trackCount, targetId: id}].concat(links.targets);
@@ -73,58 +77,61 @@ d3.json('data/' + id + '.json', function (error, links) {
     .nodes(d3.values(vertices))
     .links(links.targets)
     .size([width, height])
-    .linkDistance(function (undefined, index) {
+    .linkDistance(function () {
       return linkCount < 128 ? 128 : linkCount;
     })
-    .charge(function (undefined, index) {
+    .charge(function () {
       return linkCount < 256 ? 256 * -16 : linkCount * -16;
     })
-    .on("tick", tick)
+    .on('tick', tick)
     .start();
 
-  var svg = d3.select(".visualization").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .call(d3.behavior.zoom().scaleExtent([0.12, 16]).on("zoom", zoom))
-    .append("g");
+  var svg = d3.select('.visualization').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .call(d3.behavior.zoom().scaleExtent([0.12, 16]).on('zoom', zoom))
+    .append('g');
 
-  var background = svg.append("circle")
-    .attr("r", 2048)
-    .attr("cx", width/2)
-    .attr("cy", height/2)
-    .attr("fill", "#ddd")
-    .attr("stroke", "none");
+  // background
+  svg.append('circle')
+    .attr('r', 2048)
+    .attr('cx', width/2)
+    .attr('cy', height/2)
+    .attr('fill', '#ddd')
+    .attr('stroke', 'none');
 
-  var nodes = svg.append("g").selectAll(".node")
+  var nodes = svg.append('g').selectAll('.node')
       .data(force.nodes())
-    .enter().append("circle")
-      .attr("r", 64)
-      .attr("class", "node")
-      .on("click", function (datum, index) {
+    .enter().append('circle')
+      .attr('r', 64)
+      .attr('class', 'node')
+      .on('click', function (datum) {
         if (d3.event.defaultPrevented) {
           return;
         }
-        var transform = d3.transform(d3.select(this).attr("transform")).translate;
+        var transform = d3.transform(d3.select(this).attr('transform')).translate;
         var width = 320;
         var height = 320;
         var x = transform[0] - width/2;
         var y = transform[1] - height/2;
         var parent = d3.select(this.parentNode.parentNode);
         
-        var container = parent.append("rect")
-          .attr("width", width)
-          .attr("height", height)
-          .attr("x", x)
-          .attr("y", y)
-          .attr("class", "more-info");
-        var close = parent.append("text")
-          .attr("text-anchor", "middle")
-          .attr("class", "control")
-          .attr("dx", x + width - 16)
-          .attr("dy", y + 32)
-          .text("☓")
-          .on("click", function () {
+        var container = parent.append('rect')
+          .attr('width', width)
+          .attr('height', height)
+          .attr('x', x)
+          .attr('y', y)
+          .attr('class', 'more-info');
+
+        // x for closing  
+        parent.append('text')
+          .attr('text-anchor', 'middle')
+          .attr('class', 'control')
+          .attr('dx', x + width - 16)
+          .attr('dy', y + 32)
+          .text('☓')
+          .on('click', function () {
             if (d3.event.defaultPrevented) {
               return;
             }
@@ -133,34 +140,34 @@ d3.json('data/' + id + '.json', function (error, links) {
             name.remove();
             d3.select(this).remove();
           });
-        var name = parent.append("svg:a")
-          .attr("xlink:href", function(d) { return "?" + datum.targetId;})
-          .append("text")
-            .attr("class", "details hyperlink")
-            .attr("dx", x + 8)
-            .attr("dy", y + 32)
+        var name = parent.append('svg:a')
+          .attr('xlink:href', function() { return '?' + datum.targetId;})
+          .append('text')
+            .attr('class', 'details hyperlink')
+            .attr('dx', x + 8)
+            .attr('dy', y + 32)
             .text(datum.name);
-        var detailsText = "";
+        var detailsText = '';
         if (links.source !== datum.name) {
-          detailsText = "recorded with " + links.source + " on ";
+          detailsText = 'recorded with ' + links.source + ' on ';
         }
-        detailsText += datum.trackCount + " track" + (datum.trackCount > 1 ? "s" : "");
+        detailsText += datum.trackCount + ' track' + (datum.trackCount > 1 ? 's' : '');
 
-        var details = parent.append("text")
-            .attr("class", "details")
-            .attr("dx", x + 16)
-            .attr("dy", y + 64)
+        var details = parent.append('text')
+            .attr('class', 'details')
+            .attr('dx', x + 16)
+            .attr('dy', y + 64)
             .text(detailsText)
             .call(wrap, 303, x + 16);
       });
 
-  var text = svg.append("g").selectAll("text")
+  var text = svg.append('g').selectAll('text')
       .data(force.nodes())
-    .enter().append("text")
-      .attr("class", "name")
-      .attr("text-anchor", "middle")
+    .enter().append('text')
+      .attr('class', 'name')
+      .attr('text-anchor', 'middle')
       .text(function (d) { return d.name; })
       .call(wrap, 112);
 
-  d3.select("#progress").remove();
+  d3.select('#progress').remove();
 });
