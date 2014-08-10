6ea30837146fa4ead5ad244b75237a526f7e6a20
Rich Trott <rtrott@gmail.com>
2014-08-10 09:18:46 -0700

    move mega-closure to its own function

--- js/main.js
+++ js/main.js
@@ -105,104 +105,108 @@ d3.json('data/' + id + '.json', function (error, links) {
     .attr('fill', '#ddd')
     .attr('stroke', 'none');
 
+  var showDetails = function (datum) {
+
+    if (d3.event.defaultPrevented) {
+      return;
+    }
+    var transform = d3.transform(d3.select(this).attr('transform')).translate;
+    var width = 320;
+    var height = 320;
+    var x = transform[0] - width/2;
+    var y = transform[1] - height/2;
+    var parent = d3.select(this.parentNode.parentNode);
+    
+    var container = parent.append('rect')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('x', x)
+      .attr('y', y)
+      .attr('class', 'more-info');
+
+    // x for closing  
+    parent.append('text')
+      .attr('text-anchor', 'middle')
+      .attr('class', 'control')
+      .attr('dx', x + width - 16)
+      .attr('dy', y + 32)
+      .text('☓')
+      .on('click', function () {
+        if (d3.event.defaultPrevented) {
+          return;
+        }
+        container.remove();
+        details.remove();
+        name.remove();
+        d3.select(this).remove();
+      });
+    
+    var name = parent.append('svg:a')
+      .attr('xlink:href', function() { return '?' + datum.targetId;})
+      .append('text')
+        .attr('class', 'details-title hyperlink')
+        .attr('dx', x + 8)
+        .attr('dy', y + 32)
+        .text(datum.name);
+
+    var detailsX = x + 16;
+
+    var details = parent.append('text')
+        .attr('class', 'details')
+        .attr('dx', detailsX)
+        .attr('dy', y + 64);
+
+    var trackCountText = datum.trackCount + ' track' + (datum.trackCount > 1 ? 's' : '');
+    if (links.source === datum.name) {
+      details.append('tspan')
+        .attr('x', 0)
+        .text(trackCountText);
+    } else {
+      trackCountText += ' with ' + links.source;
+
+      details.append('tspan')
+        .attr('x', detailsX)
+        .text(trackCountText)
+        .call(wrap, 290);
+
+      details.append('tspan')
+        .attr('x', detailsX)
+        .attr('dy', '2.4em')
+        .text('including:');
+
+      var commonTrackId = datum.tracks[Math.floor(Math.random() * datum.tracks.length)];
+
+      var track = links.tracks.filter(function (elem) {
+        return elem._id === commonTrackId;
+      }).pop();
+
+      details.append('tspan')
+        .attr('x', detailsX)
+        .attr('dy', '2.4em')
+        .text('"' + track.names[0] + '"')
+        .call(wrap, 290, {x: detailsX});
+
+      details.append('tspan')
+        .attr('x', detailsX)
+        .attr('dy', '1.5em')
+        .text(track.artists[0].names[0])
+        .call(wrap, 290, {x: detailsX});
+      
+      details.append('tspan')
+        .attr('x', detailsX)
+        .attr('dy', '1.5em')
+        .attr('font-style', 'italic')
+        .text(track.releases[Math.floor(Math.random() * track.releases.length)].names[0])
+        .call(wrap, 290, {x: detailsX});
+    }
+  };
+
   var nodes = svg.append('g').selectAll('.node')
       .data(force.nodes())
     .enter().append('circle')
       .attr('r', 64)
       .attr('class', 'node')
-      .on('click', function (datum) {
-        if (d3.event.defaultPrevented) {
-          return;
-        }
-        var transform = d3.transform(d3.select(this).attr('transform')).translate;
-        var width = 320;
-        var height = 320;
-        var x = transform[0] - width/2;
-        var y = transform[1] - height/2;
-        var parent = d3.select(this.parentNode.parentNode);
-        
-        var container = parent.append('rect')
-          .attr('width', width)
-          .attr('height', height)
-          .attr('x', x)
-          .attr('y', y)
-          .attr('class', 'more-info');
-
-        // x for closing  
-        parent.append('text')
-          .attr('text-anchor', 'middle')
-          .attr('class', 'control')
-          .attr('dx', x + width - 16)
-          .attr('dy', y + 32)
-          .text('☓')
-          .on('click', function () {
-            if (d3.event.defaultPrevented) {
-              return;
-            }
-            container.remove();
-            details.remove();
-            name.remove();
-            d3.select(this).remove();
-          });
-        var name = parent.append('svg:a')
-          .attr('xlink:href', function() { return '?' + datum.targetId;})
-          .append('text')
-            .attr('class', 'details-title hyperlink')
-            .attr('dx', x + 8)
-            .attr('dy', y + 32)
-            .text(datum.name);
-
-        var detailsX = x + 16;
-
-        var details = parent.append('text')
-            .attr('class', 'details')
-            .attr('dx', detailsX)
-            .attr('dy', y + 64);
-
-        var trackCountText = datum.trackCount + ' track' + (datum.trackCount > 1 ? 's' : '');
-        if (links.source === datum.name) {
-          details.append('tspan')
-            .attr('x', 0)
-            .text(trackCountText);
-        } else {
-          trackCountText += ' with ' + links.source;
-
-          details.append('tspan')
-            .attr('x', detailsX)
-            .text(trackCountText)
-            .call(wrap, 290);
-
-          details.append('tspan')
-            .attr('x', detailsX)
-            .attr('dy', '2.4em')
-            .text('including:');
-
-          var commonTrackId = datum.tracks[Math.floor(Math.random() * datum.tracks.length)];
-
-          var track = links.tracks.filter(function (elem) {
-            return elem._id === commonTrackId;
-          }).pop();
-
-          details.append('tspan')
-            .attr('x', detailsX)
-            .attr('dy', '2.4em')
-            .text('"' + track.names[0] + '"')
-            .call(wrap, 290, {x: detailsX});
-
-          details.append('tspan')
-            .attr('x', detailsX)
-            .attr('dy', '1.5em')
-            .text(track.artists[0].names[0])
-            .call(wrap, 290, {x: detailsX});
-          
-          details.append('tspan')
-            .attr('x', detailsX)
-            .attr('dy', '1.5em')
-            .attr('font-style', 'italic')
-            .text(track.releases[Math.floor(Math.random() * track.releases.length)].names[0])
-            .call(wrap, 290, {x: detailsX});
-        }
-      });
+      .on('click', showDetails);
 
   var text = svg.append('g').selectAll('text')
       .data(force.nodes())
