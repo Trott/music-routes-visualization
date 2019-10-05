6a87e63de6f7d8292756749a7ff00944617fac89
Rich Trott <rtrott@gmail.com>
2019-10-04 19:36:50 -0700

    add standard linting

--- _tools/createDataFiles.js
+++ _tools/createDataFiles.js
@@ -2,112 +2,112 @@
 
 /* Writes files for JSON representation of all individuals on tracks with a given individual */
 
-var EventEmitter = require("events").EventEmitter;
-var argv = require("minimist")(process.argv.slice(2));
-var fs = require("graceful-fs");
+const EventEmitter = require('events').EventEmitter
+const argv = require('minimist')(process.argv.slice(2))
+const fs = require('graceful-fs')
+const path = require('path')
 
-var logger = new EventEmitter();
+var logger = new EventEmitter()
 
-if (! argv.q) {
-  logger.on("log", function (msg) {
-    console.log(msg);
-  });
+if (!argv.q) {
+  logger.on('log', function (msg) {
+    console.log(msg)
+  })
 }
 
-var sourceDataDir = __dirname + "/../node_modules/music-routes-data/data";
-var individuals = require(sourceDataDir + "/individuals.json");
-var individualIds = individuals.map(function(elem) { return elem._id; });
-var individual_track = require(sourceDataDir + "/individual_track.json");
-var tracks = require(sourceDataDir + "/tracks.json");
-var releases = require(sourceDataDir + "/releases.json");
-var track_release = require(sourceDataDir + "/track_release.json");
-var artists = require(sourceDataDir + "/artists.json");
-var artist_track = require(sourceDataDir + "/artist_track.json");
+var sourceDataDir = path.join(__dirname, '/../node_modules/music-routes-data/data')
+var individuals = require(sourceDataDir + '/individuals.json')
+var individualIds = individuals.map(function (elem) { return elem._id })
+var individualTrack = require(sourceDataDir + '/individual_track.json')
+var tracks = require(sourceDataDir + '/tracks.json')
+var releases = require(sourceDataDir + '/releases.json')
+var trackRelease = require(sourceDataDir + '/track_release.json')
+var artists = require(sourceDataDir + '/artists.json')
+var artistTrack = require(sourceDataDir + '/artist_track.json')
 
 var getName = function (id) {
-  return individuals.filter(function(elem) {
-    return elem._id === id;
-  })[0].names[0];
-};
+  return individuals.filter(function (elem) {
+    return elem._id === id
+  })[0].names[0]
+}
 
 var generateJson = function (individualId) {
-  logger.emit("log", "Generating JSON data for individual with ID of " + individualId + "...");
+  logger.emit('log', 'Generating JSON data for individual with ID of ' + individualId + '...')
 
-  var sourceLabel = getName(individualId);
+  var sourceLabel = getName(individualId)
 
-  var tracksWithIndividual = individual_track.filter(function (elem) {
-    return elem.individual_id === individualId;
+  var tracksWithIndividual = individualTrack.filter(function (elem) {
+    return elem.individual_id === individualId
   }).map(function (elem) {
-    return elem.track_id;
-  });
+    return elem.track_id
+  })
 
   var tracksDetailsForIndividual = tracks.filter(function (elem) {
-    return tracksWithIndividual.indexOf(elem._id) > -1;
-  });
+    return tracksWithIndividual.indexOf(elem._id) > -1
+  })
 
   tracksDetailsForIndividual.forEach(function (track) {
-    var trackReleaseIds = track_release.filter(function (relation) {
-      return relation.track_id === track._id;
-    }).map(function (elem) { return elem.release_id; });
+    var trackReleaseIds = trackRelease.filter(function (relation) {
+      return relation.track_id === track._id
+    }).map(function (elem) { return elem.release_id })
 
     track.releases = releases.filter(function (release) {
-      return trackReleaseIds.indexOf(release._id) > -1;
-    });
+      return trackReleaseIds.indexOf(release._id) > -1
+    })
 
-    var trackArtistIds = artist_track.filter(function (relation) {
-      return relation.track_id === track._id;
-    }).map(function (elem) { return elem.artist_id; });
+    var trackArtistIds = artistTrack.filter(function (relation) {
+      return relation.track_id === track._id
+    }).map(function (elem) { return elem.artist_id })
 
     track.artists = artists.filter(function (artist) {
-      return trackArtistIds.indexOf(artist._id) > -1;
-    });
+      return trackArtistIds.indexOf(artist._id) > -1
+    })
+  })
 
-  });
-
-  var trackCounts = {};
-  var trackArrays = {};
-  var connectedIndividuals = individual_track.filter(function (elem) {
+  var trackCounts = {}
+  var trackArrays = {}
+  var connectedIndividuals = individualTrack.filter(function (elem) {
     if (tracksWithIndividual.indexOf(elem.track_id) > -1) {
-      if (elem.individual_id != individualId) {
+      if (elem.individual_id !== individualId) {
         if (trackCounts[elem.individual_id]) {
-          trackCounts[elem.individual_id]++;
-          trackArrays[elem.individual_id].push(elem.track_id);
-          return false;
+          trackCounts[elem.individual_id]++
+          trackArrays[elem.individual_id].push(elem.track_id)
+          return false
         }
-        trackCounts[elem.individual_id] = 1;
-        trackArrays[elem.individual_id] = [elem.track_id];
-        return true;
+        trackCounts[elem.individual_id] = 1
+        trackArrays[elem.individual_id] = [elem.track_id]
+        return true
       }
     }
 
-    return false;
+    return false
   }).map(function (elem) {
     return {
       name: getName(elem.individual_id),
       targetId: elem.individual_id,
       trackCount: trackCounts[elem.individual_id],
       tracks: trackArrays[elem.individual_id]
-    };
-  });
+    }
+  })
 
-  var file = individualId + ".json";
+  var file = individualId + '.json'
   var formattedOutput = JSON.stringify({
     source: sourceLabel,
     trackCount: tracksWithIndividual.length,
     tracks: tracksDetailsForIndividual,
     targets: connectedIndividuals
-  }, null, 2);
+  }, null, 2)
 
-  fs.writeFile(__dirname + "/../data/" + file, formattedOutput,
+  fs.writeFile(path.join(__dirname, '../data', file), formattedOutput,
     function (err) {
       if (err) {
-        throw err;
+        throw err
       }
-      logger.emit("log", "Wrote JSON file for individual with ID of " + individualId);
+      logger.emit('log', 'Wrote JSON file for individual with ID of ' + individualId)
     }
-  );
-};
+  )
+}
 
 individualIds.forEach(function (individualId) {
-  generateJson(individualId);
-});
+  generateJson(individualId)
+})
--- js/main.js
+++ js/main.js
@@ -1,186 +1,185 @@
 /* global window */
 /* global d3 */
-var id = window.location.search.substring(1).replace(/\W/g, '') || '1';
+var id = window.location.search.substring(1).replace(/\W/g, '') || '1'
 d3.json('data/' + id + '.json', function (error, links) {
   if (error) {
-    d3.select('.visualization').remove();
-    var body = d3.select('body');
-    body.append('p').text('Something went wrong.');
-    body.append('p').text('Check your network connection and reload the page.');
-    body.append('p').text('If the problem persists, you can email rtrott@gmail.com and I\'ll try to fix it.');
-    body.append('p').text('If you do that, please include as much detail as you can:');
-    var ul = body.append('ul');
-    ul.append('li').text('what you did leading up to the problem');
-    ul.append('li').text('the current URL of this page');
-    ul.append('li').text('anything else you might think would be relevant');
-    body.append('p').text('Thanks!');
-    return error;
+    d3.select('.visualization').remove()
+    var body = d3.select('body')
+    body.append('p').text('Something went wrong.')
+    body.append('p').text('Check your network connection and reload the page.')
+    body.append('p').text('If the problem persists, you can email rtrott@gmail.com and I\'ll try to fix it.')
+    body.append('p').text('If you do that, please include as much detail as you can:')
+    var ul = body.append('ul')
+    ul.append('li').text('what you did leading up to the problem')
+    ul.append('li').text('the current URL of this page')
+    ul.append('li').text('anything else you might think would be relevant')
+    body.append('p').text('Thanks!')
+    return error
   }
 
   var wrap = function (text, width, options) {
-    options = options || {};
-    options.x = options.x || 0;
-    options.mody = options.mody || false; // whether or not to scoot text up (modify the y) with each line wrapped
-    text.each(function() {
-      var text = d3.select(this),
-        words = text.text().split(/\s+/).reverse(),
-        word,
-        line = [],
-        y = text.attr('y'),
-        tspan = text.text(null).append('tspan').attr('x', options.x);
-      
+    options = options || {}
+    options.x = options.x || 0
+    options.mody = options.mody || false // whether or not to scoot text up (modify the y) with each line wrapped
+    text.each(function () {
+      var text = d3.select(this)
+      var words = text.text().split(/\s+/).reverse()
+      var word
+      var line = []
+      var y = text.attr('y')
+      var tspan = text.text(null).append('tspan').attr('x', options.x)
+
       while (words.length) {
-        word = words.pop();
-        line.push(word);
-        tspan.text(line.join(' '));
+        word = words.pop()
+        line.push(word)
+        tspan.text(line.join(' '))
         if (tspan.node().getComputedTextLength() > width && line.length > 1) {
           if (options.mody) {
-            y = y-10;
+            y = y - 10
           }
-          text.attr('y', y);
-          line.pop();
-          tspan.text(line.join(' '));
-          line = [word];
-          tspan = text.append('tspan').attr('x', options.x).attr('dy', 18).text(word);
+          text.attr('y', y)
+          line.pop()
+          tspan.text(line.join(' '))
+          line = [word]
+          tspan = text.append('tspan').attr('x', options.x).attr('dy', 18).text(word)
         }
       }
-    });
-  };
+    })
+  }
 
   var transform = function (d) {
-    return 'translate(' + d.x + ',' + d.y + ')';
-  };
+    return 'translate(' + d.x + ',' + d.y + ')'
+  }
 
   var textTransform = function (d) {
-    var y = d.y + 8;
-    return 'translate(' + d.x + ',' + y + ')';
-  };
+    var y = d.y + 8
+    return 'translate(' + d.x + ',' + y + ')'
+  }
 
   var tick = function () {
-    nodes.attr('transform', transform);
-    text.attr('transform', textTransform);
-  };
+    nodes.attr('transform', transform)
+    text.attr('transform', textTransform)
+  }
 
   var zoom = function () {
-    svg.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
-  };
+    svg.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')')
+  }
 
-  var vertices = [{name: links.source, trackCount: links.trackCount, targetId: id}].concat(links.targets);
+  var vertices = [{ name: links.source, trackCount: links.trackCount, targetId: id }].concat(links.targets)
 
-  links.targets.forEach(function(link, index) {
-    link.source = vertices[0];
-    link.target = vertices[index+1];
-  });
+  links.targets.forEach(function (link, index) {
+    link.source = vertices[0]
+    link.target = vertices[index + 1]
+  })
 
-  var linkCount = links.targets.length;
+  var linkCount = links.targets.length
 
-  var width = window.innerWidth || 960,
-      height = window.innerHeight || 500;
+  var width = window.innerWidth || 960
+  var height = window.innerHeight || 500
 
   var force = d3.layout.force()
     .nodes(d3.values(vertices))
     .links(links.targets)
     .size([width, height])
     .linkDistance(function () {
-      return linkCount < 128 ? 128 : linkCount;
+      return linkCount < 128 ? 128 : linkCount
     })
     .charge(function () {
-      return linkCount < 256 ? 256 * -16 : linkCount * -16;
+      return linkCount < 256 ? 256 * -16 : linkCount * -16
     })
     .on('tick', tick)
-    .start();
+    .start()
 
   var svg = d3.select('.visualization').append('svg')
     .attr('width', width)
     .attr('height', height)
     .append('g')
     .call(d3.behavior.zoom().scaleExtent([0.12, 16]).on('zoom', zoom))
-    .append('g');
+    .append('g')
 
   // background
   svg.append('circle')
     .attr('r', 2048)
-    .attr('cx', width/2)
-    .attr('cy', height/2)
+    .attr('cx', width / 2)
+    .attr('cy', height / 2)
     .attr('fill', '#ddd')
-    .attr('stroke', 'none');
+    .attr('stroke', 'none')
 
   var showDiscography = function (datum) {
-    var isSource = datum.name === links.source;
+    var isSource = datum.name === links.source
 
-    d3.select('.visualization').style({display: 'none'});
+    d3.select('.visualization').style({ display: 'none' })
 
     // x for closing
-    var container = d3.select('.discography');
+    var container = d3.select('.discography')
     container.append('button')
       .attr('class', 'control')
       .text('☓')
       .on('click', function () {
         if (d3.event.defaultPrevented) {
-          return;
+          return
         }
 
-        d3.select('.visualization').style({display: null});
-        d3.select('.discography').html('');
-      });
+        d3.select('.visualization').style({ display: null })
+        d3.select('.discography').html('')
+      })
 
-    var h1 = container.append('h1');
+    var h1 = container.append('h1')
     if (isSource) {
-      h1.text(datum.name + ' tracks');
+      h1.text(datum.name + ' tracks')
     } else {
-      h1.append('a').attr('href', '?' + datum.targetId).text(datum.name);
-      h1.append('span').text('/' + links.source + ' tracks');
+      h1.append('a').attr('href', '?' + datum.targetId).text(datum.name)
+      h1.append('span').text('/' + links.source + ' tracks')
     }
-    var table = container.append('table').style({'border-collapse': 'collapse'});
-    var row = table.append('tr');
-    row.append('th').text('Track').style('border', '1px solid');
-    row.append('th').text('Artist').style('border', '1px solid');
-    row.append('th').text('Release').style('border', '1px solid');
-    
-    var tracks;
+    var table = container.append('table').style({ 'border-collapse': 'collapse' })
+    var row = table.append('tr')
+    row.append('th').text('Track').style('border', '1px solid')
+    row.append('th').text('Artist').style('border', '1px solid')
+    row.append('th').text('Release').style('border', '1px solid')
+
+    var tracks
     if (isSource) {
-      tracks = links.tracks;
+      tracks = links.tracks
     } else {
-      tracks = links.tracks.filter( function (elem) {
-        return datum.tracks.indexOf(elem._id) !== -1;
-      });
+      tracks = links.tracks.filter(function (elem) {
+        return datum.tracks.indexOf(elem._id) !== -1
+      })
     }
 
-    for (var i=0, l=tracks.length; i<l; i++) {
-      row = table.append('tr');
-      var track = tracks[i];
+    for (var i = 0, l = tracks.length; i < l; i++) {
+      row = table.append('tr')
+      var track = tracks[i]
       // TODO: append each name etc. separated by semi-colons
-      row.append('td').text('"' + track.names[0] + '"');
-      row.append('td').text(track.artists[0].names[0]);
-      row.append('td').text(track.releases[0].names[0]).style('font-style', 'italic');
+      row.append('td').text('"' + track.names[0] + '"')
+      row.append('td').text(track.artists[0].names[0])
+      row.append('td').text(track.releases[0].names[0]).style('font-style', 'italic')
     }
-    
-    table.selectAll('td').style({border: '1px solid', 'text-align': 'left', padding: '0.5em'});
-  };
+
+    table.selectAll('td').style({ border: '1px solid', 'text-align': 'left', padding: '0.5em' })
+  }
 
   var showDetails = function (datum) {
-
     if (d3.event.defaultPrevented) {
-      return;
+      return
     }
 
-    showDiscography(datum);
-  };
+    showDiscography(datum)
+  }
 
   var nodes = svg.append('g').selectAll('.node')
-      .data(force.nodes())
+    .data(force.nodes())
     .enter().append('circle')
-      .attr('r', 64)
-      .attr('class', 'node')
-      .on('click', showDetails);
+    .attr('r', 64)
+    .attr('class', 'node')
+    .on('click', showDetails)
 
   var text = svg.append('g').selectAll('text')
-      .data(force.nodes())
+    .data(force.nodes())
     .enter().append('text')
-      .attr('class', 'name')
-      .attr('text-anchor', 'middle')
-      .text(function (d) { return d.name; })
-      .call(wrap, 112, {mody: true});
+    .attr('class', 'name')
+    .attr('text-anchor', 'middle')
+    .text(function (d) { return d.name })
+    .call(wrap, 112, { mody: true })
 
-  d3.select('#progress').remove();
-});
+  d3.select('#progress').remove()
+})
